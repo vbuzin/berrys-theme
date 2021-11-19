783353babbf2c719fc5607108756a4bcab758cba
Slava Buzin <v8v.buzin@gmail.com>
2021-11-19 10:25:02 +0100

    v2

---
+++ berrys-dark-theme.el
@@ -0,0 +1,12 @@
+;;; berrys-dark-theme.el - dark version -*- lexical-binding: t; -*-
+;;; Code:
+
+(require 'berrys-theme)
+
+(deftheme berrys-dark "A mostly monochrome dark theme")
+
+(berrys-create 'dark 'berrys-dark)
+
+(provide-theme 'berrys-dark)
+
+;;; berrys-dark-theme.el ends here
---
+++ berrys-light-theme.el
@@ -0,0 +1,12 @@
+;;; berrys-light-theme.el - light version -*- lexical-binding: t; -*-
+;;; Code:
+
+(require 'berrys-theme)
+
+(deftheme berrys-light "A mostly monochrome light theme")
+
+(berrys-create 'light 'berrys-light)
+
+(provide-theme 'berrys-light)
+
+;;; berrys-light-theme.el ends here
